998659b5ff04dfadc3a057bbcc44337fe2915ad2
Hitarth <84812700+brohudev@users.noreply.github.com>
2024-10-06 10:06:16 -0500

    solve the handle_question_query problem

--- src/command_handlers.py
+++ src/command_handlers.py
@@ -1,8 +1,8 @@
 from mongo import users_collection
 from mongo import ask_question , get_random_question 
 import discord
-from discord.ext import commands
-    
+import utils
+
 def handle_leaderboard_query():
     users = list(users_collection.find().sort('points', -1).limit(10))
     
@@ -25,23 +25,5 @@ def handle_question_query():
     choices = random_question['choices']
     correct_answer = random_question['correct_answer']
     
-    async def send_question(ctx):
-        def check(m):
-            return m.author == ctx.author and m.content in choices
-        
-        await ctx.send(question_text)
-        for choice in choices:
-            await ctx.send(choice)
-        
-        try:
-            msg = await bot.wait_for('message', check=check, timeout=60.0)
-        except asyncio.TimeoutError:
-            await ctx.send('Sorry, you took too long to respond.')
-        else:
-            if msg.content == correct_answer:
-                ask_question(random_question)
-                await ctx.send("Correct! You have selected the correct answer!")
-            else:
-                await ctx.send("Incorrect. Sorry, that's not the correct answer.")
     
-    return send_question
+    return f'{question_text}: {choices}; {correct_answer}'
