60c6b55b3d0619d4533787ce31f85fad8bc793c1
decloon~ <decstaun06@outlook.com~>
2024-10-06 02:03:19 -0500

    Da bot works

--- main.py
+++ main.py
@@ -21,3 +21,27 @@ async def send_message(message: Message, user_message: str) -> None:
     if is_private:
         user_message = user_message[1:]
 
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+#Prevents bot from responding to itself
+@client.event
+async def on_message(message) -> None:
+    if message.author == client.user:
+        return
+
+    username = str(message.author)
+    usermessage = message.content
+    channel = message.channel
+
+    print(f'[{channel}] {username}: {usermessage}')
+    await send_message(message)
+
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+    main()
