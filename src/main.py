2273294502d93e9d04a481c72b91c510a593d0b8
Hitarth <84812700+brohudev@users.noreply.github.com>
2024-10-06 08:58:43 -0500

    add gui functionality to the !question handler

--- src/main.py
+++ src/main.py
@@ -55,10 +55,15 @@ async def send_message(message: Message, usermessage: str, channel) -> None:
     if usermessage[0] == "!":
         return "nothing"
 
+# Signal handler to gracefully shutdown the bot
+def signal_handler(signal, frame):
+    print("Shutting down...")
+    client.close()
 
 # Main function to run the bot
 def main() -> None:
-    client.run(token=TOKEN)
+    signal.signal(signal.SIGINT, signal_handler)
+    client.run(TOKEN)
 
 if __name__ == '__main__':
     main()
